eccadc5e820d18b09eca096f5a29e4d2c5e799e4
Arshdeep Singh <singh_arsy@yahoo.com>
2020-04-15 15:49:40 -0400

    add statement and whitespacing java project

---
+++ StatementsWhiteSpaceAndIndenting/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("this is a complete java statement");
+
+        System.out.println("this is +" +
+                "another" +
+                "still more");
+
+        int anotherVariable = 50; myVariable--;System.out.println("this is another one:");
+    }
+}
